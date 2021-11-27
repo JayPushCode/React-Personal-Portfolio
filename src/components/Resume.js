3dbd06a9bf3cea0debfa86700751edbdd0ffc412
Jay <jaypushcode@gmail.com>
2021-11-26 23:43:06 -0500

    Adding Resume Component & File

---
+++ src/components/Resume.js
@@ -0,0 +1,10 @@
+import '../app.css';
+
+function Resume () {
+    return
+    <div>
+        <embed src="./Projects/2021ResumeWFJ.pdf" type="application/pdf" width="100%" height="100%" />
+    </div>
+}
+
+export default Resume
