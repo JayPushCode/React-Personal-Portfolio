516bd13a802acf152d180c7ece4cb041dc4a5e06
Jay <jaypushcode@gmail.com>
2021-12-03 15:54:14 -0500

    Updating AppliTrak Link

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -10,7 +10,7 @@ function Projects() {
       <h2 className="mb-5">Github Projects</h2>
       <section id="Portfolio-work">
         <a className="ProjectTemplate sample-img" id="Project1" href="https://github.com/JayPushCode/FEEDIT" target="_blank" rel="noopener noreferrer">  </a>
-        <a className="ProjectTemplate sample-img2" href="https://immense-eyrie-11789.herokuapp.com/login" target="_blank" rel="noopener noreferrer">  </a>
+        <a className="ProjectTemplate sample-img2" href="https://github.com/JayPushCode/job-application-tracker" target="_blank" rel="noopener noreferrer">  </a>
         <a className="ProjectTemplate sample-img3" href="https://github.com/jnordan132/Proj-3" target="_blank" rel="noopener noreferrer">  </a>
         {/* <a className="ProjectTemplate sample-img4" href="https://github.com/JayPushCode/WorkDayScheduler" target="_blank" rel="noopener noreferrer"> Workday Scheduler </a> */}
         {/* <a className="ProjectTemplate sample-img5" href="https://github.com/JayPushCode/JS-Code-Quiz" target="_blank" rel="noopener noreferrer"> Javascript Coding Quiz </a> */}
