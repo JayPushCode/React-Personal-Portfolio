0859ce3df866b1d9f20678c3d8fb4a0434515d07
monty <jaypushcode@gmail.com>
2024-01-17 17:54:55 -0500

    Adding new work experience

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -8,6 +8,25 @@ function Experience() {
   <section className="resume-section" id="experience">
     <div className="resume-section-content">
       <h2 className="mb-5">Experience</h2>
+      
+      <div className="d-flex flex-column flex-md-row justify-content-between mb-5">
+        <div className="flex-grow-1">
+          <h3 className="mb-0">Software Developer </h3>
+          <div className="subheading mb-3"> Integrity Support, Inc.</div>
+          <p> Worked in a sophisticated MVC/MVVM .NET Pattern EHR application. Engaged in both front-end and back-end development using C#, Classic ASP, and Javascript to enhance user experiences. Crafted stored procedures and scripts in Microsoft SQL Server for efficient customer data management. Innovatively optimized SQL table structures by implementing custom arrays with JSON and jQuery. Created a cutting-edge client portal website with integrated chat, signature-capturing, and document-sharing functionalities, ensuring compliance with modern EHR regulations. Successfully deployed and tested using Azure CI/CD tools on Cloud Services with Microsoft IIS sites on a cloud Windows Server. </p>
+        </div>
+        <div className="flex-shrink-0"><span className="text-primary"> March 2022 - January 2024 </span></div>
+      </div>
+      
+      <div className="d-flex flex-column flex-md-row justify-content-between mb-5">
+        <div className="flex-grow-1">
+          <h3 className="mb-0">Tutor </h3>
+          <div className="subheading mb-3"> edX / 2U </div>
+          <p> Instructed over 100 students in the foundational principles of full-stack web development. Covered a spectrum of topics, spanning fundamental elements like HTML, CSS, and Javascript, to advanced skills such as constructing client-side applications utilizing third-party APIs through Fetch Calls. Additionally, provided comprehensive guidance on both MySQL and NoSQL databases, emphasizing the utilization of back-end Node JS technologies like Sequelize and Mongoose. The ultimate objective was to equip students with the proficiency to implement React in developing sophisticated MERN stack single-page applications (MongoDB, Express JS, React JS, Node JS). </p>
+        </div>
+        <div className="flex-shrink-0"><span className="text-primary"> May 2022 - January 2024 </span></div>
+      </div>
+
       <div className="d-flex flex-column flex-md-row justify-content-between mb-5">
         <div className="flex-grow-1">
           <h3 className="mb-0">Insurance Sales Representative</h3>
