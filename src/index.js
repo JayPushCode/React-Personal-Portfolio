57e0cc36a6b2a63edbbfa2d59d35c9b5b3e7ff45
monty <jaypushcode@gmail.com>
2024-01-19 15:56:25 -0500

    Updating React Root file

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
+// import reportWebVitals from 'react-dom';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-// import reportWebVitals from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('root');
+const root = createRoot(container);
+ // createRoot(container!) if you use TypeScript
+root.render(<App />);
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
