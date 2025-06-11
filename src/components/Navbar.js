e9a71f6f67bb6d122ee941d5488ee02e2b7035be
uyo <jaypushcode@gmail.com>
2025-06-10 21:44:29 -0400

    Fixing Nav bar class w/ state

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,60 @@
+import React, { useState, useEffect } from 'react';
 import '../app.css';
-import profile from "../Projects/profile.jpg"
+import profile from "../Projects/profile.jpg";
+
+const sections = ["about", "experience", "education", "awards", "skills", "interests"];
 
 function Navbar() {
+  const [activeSection, setActiveSection] = useState("");
+
+  useEffect(() => {
+    const handleScroll = () => {
+      let foundSection = "";
+      for (let id of sections) {
+        const element = document.getElementById(id);
+        if (element) {
+          const rect = element.getBoundingClientRect();
+          if (rect.top <= 100 && rect.bottom >= 100) {
+            foundSection = id;
+            break;
+          }
+        }
+      }
+      setActiveSection(foundSection);
+    };
+
+    window.addEventListener("scroll", handleScroll, { passive: true });
+    handleScroll(); // initial call
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+      <a className="navbar-brand js-scroll-trigger" href="#page-top">
+        <span className="d-none d-lg-block">
+          <img className="img-fluid img-profile rounded-circle mx-auto mb-2" src={profile} alt="Profile" />
+        </span>
+      </a>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon" />
+      </button>
+      <div className="collapse navbar-collapse" id="navbarResponsive">
+        <ul className="navbar-nav">
+          {sections.map((section) => (
+            <li className="nav-item" key={section}>
+              <a
+                className={`nav-link js-scroll-trigger ${activeSection === section ? "active" : ""}`}
+                href={`#${section}`}
+              >
+                {section === "interests" ? "Github Projects" : section.charAt(0).toUpperCase() + section.slice(1)}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+}
 
-<nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
-  <a className="navbar-brand js-scroll-trigger" href="#page-top">
-    {/* <span class="d-block d-lg-none">Wongani F. Jere</span> */}
-    <span className="d-none d-lg-block"><img className="img-fluid img-profile rounded-circle mx-auto mb-2" src={profile} alt="..." /></span>
-  </a>
-  <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon" /></button>
-  <div className="collapse navbar-collapse" id="navbarResponsive">
-    <ul className="navbar-nav">
-      <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#about">About</a></li>
-      <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#experience">Experience</a></li>
-      <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#education">Education</a></li>
-      <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#awards">Awards</a></li>
-      <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#skills">Skills</a></li>
-      <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#interests">Github Projects</a></li>
-    </ul>
-  </div>
-</nav>
-  )
-  };
-
-export default Navbar; 
+export default Navbar;
